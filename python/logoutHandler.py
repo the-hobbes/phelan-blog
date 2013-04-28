dda809684e27241543690f94ad0f25c4d04c0dce
the-hobbes <phelan.vendeville@gmail.com>
2013-04-28 11:12:13 -0400

    fixed styling issue with sidebar

--- python/logoutHandler.py
+++ python/logoutHandler.py
@@ -20,8 +20,11 @@ from python.handler import *
 class LogoutHandler(Handler):
 	def get(self):
 		# get the login cookie user_id and set it to nothing, then redirect to the signup page
-		if self.request.cookies.get('user_id'):
-			newCookieVal = ""
-			self.response.headers.add_header("Set-Cookie", "user_id=%s; Path=/" % newCookieVal)
 
-		self.redirect("/signup")
+		if self.request.cookies.get('user_id'):
+			# the way you log someone out is to delete their cookie, or in this case set it to an empty string
+			# newCookieVal = ""
+			# self.response.headers.add_header("Set-Cookie", "user_id=%s; Path=/" % newCookieVal)
+			self.logout()
+
+		self.redirect("/")
