1c6659ca63c1e23f670fc1d51fa43fd0bd48fc70
the-hobbes <phelan.vendeville@gmail.com>
2013-04-28 21:00:20 -0400

    added print style

--- main.py
+++ main.py
@@ -13,7 +13,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
+
+'''
+	This is the main request handler, as well as the url to handler mapping.
+'''
+
 import webapp2
 from python.handler import *
 from python.signupHandler import *
