c3faf2f9800646ff457afde152a7c890318eb386
the-hobbes <phelan.vendeville@gmail.com>
2013-05-16 20:49:49 -0400

    added caching to permalink pages, as well as cache age display

--- python/permalinkHandler.py
+++ python/permalinkHandler.py
@@ -21,13 +21,25 @@ import datastore
 class PermalinkHandler(Handler):
 	def get(self, entryId):
 		# has the id of the entry passed along with the get request so it can be looked up and displayed
-		blog_entry = Posts.get_by_id(int(entryId))
+		# blog_entry = Posts.get_by_id(int(entryId))
+
+		# make a post key for the memcache
+		postKey = "POST_" + entryId
+
+		# check to see if that post is already in the cache. if its is, we have the age
+		blog_entry, age = ageGet(postKey)
+
+		# if it isn't then we add it to the cache by looking it up from the database
+		if not blog_entry:
+			blog_entry = Posts.get_by_id(int(entryId))
+			ageSet(postKey, blog_entry)
+			age = 0
 
 		if blog_entry:
-			self.render("permalink.html", blog_entry=blog_entry)
+			self.render("permalink.html", blog_entry=blog_entry, elapsedTime=ageStr(age))
 		else:
-			self.render("permalink.html", blog_entry="", error="Blog post %s not found!" % entryId)
+			self.render("permalink.html", blog_entry="", elapsedTime=ageStr(age), error="Blog post %s not found!" % entryId)
 
 	def post(self):
-		# no post
+		# no post method here no sir
 		pass
